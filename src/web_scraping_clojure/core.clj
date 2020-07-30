0f6ebd0c027c54e3a33231a4fc1977eea00f5d78
WickedBrat <harshsrivastav123@gmail.com>
2020-07-30 15:19:43 +0530

    Completed scrape function

--- src/web_scraping_clojure/core.clj
+++ src/web_scraping_clojure/core.clj
@@ -9,6 +9,22 @@
   (println (map (fn [category] category) (keys category-list)))
 )
 
+(defn parse-page-numbers
+  [list-of-page-numbers]
+  (map (fn [page-number]
+         (if (= page-number 1)
+         "/index.html"
+         (str "/page-" page-number ".html"))) list-of-page-numbers)
+)
+
+(defn scrape
+  [category-args]
+
+  (println category-args)
+  (map (fn [category-arg]
+         (let [url (get category-list (name (key category-arg)))]
+           (http-service/fetch-books url (parse-page-numbers (val category-arg))))) category-args))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- src/web_scraping_clojure/process_http.clj
+++ src/web_scraping_clojure/process_http.clj
@@ -7,13 +7,53 @@
 
 (defn get-html-dom
   [url]
+  (println "sending req to: " url)
   (html/html-snippet
    (:body @(http/get (str "http://books.toscrape.com/" url) {:insecure? true}))))
+
 
 (defn extract-titles
   [dom]
   (apply hash-map (reduce into (map
-   (fn [key] [(strings/trim (apply str (get key :content))), ((key :attrs) :href)]) (html/select dom [:ul.nav :li :ul :li :a])))))
+   (fn [key] [(strings/lower-case (strings/trim (apply str (get key :content)))), ((key :attrs) :href)])
+   (html/select dom [:ul.nav :li :ul :li :a])))))
+
+(defn parse-page-for-books-url
+  [page-dom]
+  (map
+    (fn [key] ((key :attrs) :href))
+    (html/select page-dom [:article.product_pod :h3 :a]))
+)
+
+(defn parse-book-details
+  [book-page-dom]
+  (let [title (map
+               (comp first :content)
+               (html/select book-page-dom [:div.product_main :h1]))]
+    (let [description (map
+                 (comp first :content)
+                 (html/select book-page-dom [:article.product_page :p]))]
+      {:title title, :description description}))
+)
+
+(defn url-without-page
+  [url]
+  (strings/join "/" (drop-last (strings/split url #"/"))))
+
+(defn get-url
+  [url page-number]
+  (str (url-without-page url) page-number)
+)
+
+(defn fetch-books
+  [url pages]
+  (println url pages)
+  (let [books-urls (pmap (fn [page-number]
+                           (parse-page-for-books-url (get-html-dom (get-url url page-number)))) pages)]
+    
+    (pmap (fn [book-url]
+            (parse-book-details (get-html-dom (str (url-without-page url) "/" book-url)))) (first books-urls)))
+)
 
 (defn initialize-list
   []
