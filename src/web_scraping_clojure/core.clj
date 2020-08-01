934f8c6108221cd4a9df5cd515a88691b19fc13c
giridharavinash <giridharavinash93@gmail.com>
2020-08-01 18:44:10 +0530

    Added extra categories to scrape function

--- src/web_scraping_clojure/core.clj
+++ src/web_scraping_clojure/core.clj
@@ -3,7 +3,7 @@
   (:gen-class))
 
 (def category-list (http-service/initialize-list))
-
+; (def a :1-2)
 (defn list-categories
   []
   (println (map (fn [category] category) (keys category-list)))
@@ -11,10 +11,18 @@
 
 (defn parse-page-numbers
   [list-of-page-numbers]
+  
   (map (fn [page-number]
+       
+        (if (not (keyword? page-number))
          (if (= page-number 1)
-         "/index.html"
-         (str "/page-" page-number ".html"))) list-of-page-numbers)
+          "/index.html"
+         (str "/page-" page-number ".html"))
+         (if (not (= page-number :all))
+          (parse-page-numbers (range (Integer. (get (clojure.string/split (get (clojure.string/split (str page-number) #"-") 0) #":") 1))  (+ (Integer.(get (clojure.string/split (str page-number) #"-") 1)) 1)))
+          )
+         )
+         ) list-of-page-numbers)
 )
 
 (defn scrape
@@ -23,10 +31,12 @@
   (println category-args)
   (map (fn [category-arg]
          (let [url (get category-list (name (key category-arg)))]
-           (http-service/fetch-books url (parse-page-numbers (val category-arg))))) category-args))
+           (http-service/fetch-books url (flatten (parse-page-numbers (val category-arg)) )  ))) category-args))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Please enter through repl to run all the commands.")
 )
+
+
